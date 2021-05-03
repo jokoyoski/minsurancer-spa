8e70a6e42ce71ffc9ac8983ae5bee6afc8c4c968
Mohammed Agboola <magboola@inlaks.com>
2021-05-03 07:28:00 +0100

    ⚡️ task: remove sidebar scroll on re-render

--- src/components/pages/SideBar/SubMenu.jsx
+++ src/components/pages/SideBar/SubMenu.jsx
@@ -1,64 +1,66 @@
-import { Link } from "react-router-dom";
-import React, { useState, useRef, useEffect } from "react";
-import "../SideBar/side-menu.styles.scss";
+import { Link } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+import '../SideBar/side-menu.styles.scss';
 import history from '../../../router/browserrouter';
 import { withRouter } from 'react-router-dom';
-export const SubMenu = (props) => {
-  console.log(props)
+export const SubMenu = props => {
+  console.log(props);
   const [buttonIdsArray, setIds] = useState([]);
-  const [path, setPath] = useState('')
+  const [path, setPath] = useState('');
   const isMounted = useRef(false);
   for (var i = 0; i < props.length; i++) {
     setIds(subNav.id);
   }
 
-  const showSubNav = (id) => {
+  const showSubNav = id => {
     let i = 0;
     for (i = 0; i < buttonIdsArray.length; i++) {
       if (buttonIdsArray[i] !== id) {
         var el = document.getElementById(buttonIdsArray[i]);
-        el.style.maxHeight = "0px";
+        el.style.maxHeight = '0px';
       }
     }
     el = document.getElementById(id);
-    if (el.style.maxHeight !== "0px") {
-      el.style.maxHeight = "0px";
+    if (el.style.maxHeight !== '0px') {
+      el.style.maxHeight = '0px';
     } else {
       el.style.maxHeight = `${content.current.scrollHeight}px`;
     }
   };
 
-
   const content = useRef(null);
   const { title, subNav, icon, id } = props;
 
   useEffect(() => {
-    if (+localStorage.getItem("current-nav") === id) {
+    if (+localStorage.getItem('current-nav') === id) {
       showSubNav(id);
-
-
     }
   }, []);
+
   return (
     <div>
       <div
-        className="menu-bar"
+        className='menu-bar'
         style={{
-          display: "flex",
-          fontFamily: "Muli",
-          cursor: "pointer",
-          fontSize: "0.9rem",
-          marginTop: "0.4rem",
-          padding: "8px",
+          display: 'flex',
+          fontFamily: 'Muli',
+          cursor: 'pointer',
+          fontSize: '0.9rem',
+          marginTop: '0.4rem',
+          padding: '8px'
         }}
-        onClick={(e) => {
+        onClick={e => {
           e.preventDefault();
           showSubNav(id);
-          localStorage.setItem("current-nav", id);
-        }}
-      >
+          localStorage.setItem('current-nav', id);
+        }}>
         <span style={{ color: 'rgb(164, 166, 179)' }}>{icon}</span>
-        <span style={{ display: "inline-block", marginLeft: "3px", color: 'rgb(164, 166, 179)' }}>
+        <span
+          style={{
+            display: 'inline-block',
+            marginLeft: '3px',
+            color: 'rgb(164, 166, 179)'
+          }}>
           {title}
         </span>
       </div>
@@ -66,16 +68,21 @@ export const SubMenu = (props) => {
         id={id}
         ref={content}
         style={{
-          maxHeight: "0px",
-          overflowX: "hidden",
-          transition: `max-height 1.0s`,
-        }}
-      >
+          maxHeight: '0px',
+          overflowX: 'hidden'
+        }}>
         {subNav.map((item, index) => {
           return (
-            <Link  className={localStorage.getItem("currentPath") === item.path ? 'side-bar-link active-yo' : 'side-bar-link'} to={item.path} key={item.title}>
+            <Link
+              className={
+                localStorage.getItem('currentPath') === item.path
+                  ? 'side-bar-link active-yo'
+                  : 'side-bar-link'
+              }
+              to={item.path}
+              key={item.title}>
               <span>{item.icon}</span>
-              <span className="side-bar-label">{item.title}</span>
+              <span className='side-bar-label'>{item.title}</span>
             </Link>
           );
         })}
