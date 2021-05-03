1f6b0ed7fd803e74c5f75be711a64d15b5e862ae
Mohammed Agboola <magboola@inlaks.com>
2021-05-03 15:53:50 +0100

    🚑 hotfix: set active sidebar link

--- src/components/pages/SideBar/SubMenu.jsx
+++ src/components/pages/SideBar/SubMenu.jsx
@@ -79,6 +79,7 @@ export const SubMenu = props => {
                   ? 'side-bar-link active-yo'
                   : 'side-bar-link'
               }
+              onClick={() => localStorage.setItem('currentPath', item.path)}
               to={item.path}
               key={item.title}>
               <span>{item.icon}</span>
